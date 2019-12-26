c3d24d218d206ccb9726e62f1d24579cc1e278fc
Ushinji <siguma971@gmail.com>
2019-12-26 21:50:24 +0900

    Setup cecium, recium using webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+import 'cesium/Widgets/widgets.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
