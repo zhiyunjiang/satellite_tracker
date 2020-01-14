90dd48b5f4e19f3c0a1ee31ee46d9abd1c127ede
Ushinji <siguma971@gmail.com>
2020-01-14 19:56:30 +0900

    Add move clock.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { Viewer, Globe } from 'resium';
+import { Viewer, Globe, Clock } from 'resium';
 import SateliteOrbital from './components/SateliteOrbital';
 
 const App: React.FC = () => {
   return (
     <Viewer full>
       <Globe enableLighting />
+      <Clock shouldAnimate />
       <SateliteOrbital />
     </Viewer>
   );
