c31951fa82306ca50b78f22b72432f45ad941a1c
Ushinji <siguma971@gmail.com>
2020-01-14 19:46:15 +0900

    Add component `SateliteOrbital`

--- src/App.tsx
+++ src/App.tsx
@@ -1,114 +1,12 @@
 import * as React from 'react';
-import { Cartesian3 } from 'cesium';
-import { Viewer, CzmlDataSource, CameraFlyTo, Globe } from 'resium';
-import { getOrbital, GetOrbitalResponse } from './queries/orbitalQuery';
-
-const { useState, useCallback, useEffect } = React;
-
-type OrbitalState = {
-  isLoading: boolean;
-  isError: boolean;
-  response?: GetOrbitalResponse;
-};
-
-const useOrbital = () => {
-  const [state, setState] = useState<OrbitalState>({
-    isLoading: true,
-    isError: false,
-    response: undefined,
-  });
-
-  const fetchData = useCallback(async () => {
-    const res = await getOrbital();
-    if (res.status >= 400) {
-      setState(prevState => {
-        return { ...prevState, isLoading: false, isError: true };
-      });
-    }
-    const data = res.data as GetOrbitalResponse;
-    setState(prevState => {
-      return {
-        ...prevState,
-        isLoading: false,
-        isError: false,
-        response: data,
-      };
-    });
-  }, []);
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
-
-  return { ...state };
-};
+import { Viewer, Globe } from 'resium';
+import SateliteOrbital from './components/SateliteOrbital';
 
 const App: React.FC = () => {
-  const { isError, isLoading, response } = useOrbital();
-  if (isLoading) return <div>Loading....</div>;
-  if (isError || !response) return <div>Error!</div>;
-
-  const czml = [
-    {
-      id: 'document',
-      name: 'CZML',
-      version: '1.0',
-      clock: {
-        currentTime: response.epoch,
-      },
-    },
-    {
-      id: 'Satellite/ISS',
-      name: 'ISS',
-      availability: response.availability,
-      description: '<p>The International Space Station (ISS)</p>',
-      ellipse: {
-        semiMinorAxis: 50000.0,
-        semiMajorAxis: 50000.0,
-        height: response.position.alt,
-        material: {
-          solidColor: {
-            color: {
-              rgba: [9, 175, 237, 255],
-            },
-          },
-        },
-        outline: true, // height must be set for outlines to display
-        outlineColor: {
-          rgba: [255, 255, 255, 255],
-        },
-      },
-      path: {
-        show: [
-          {
-            interval: response.availability,
-            boolean: true,
-          },
-        ],
-        width: 1,
-        material: {
-          solidColor: {
-            color: {
-              rgba: [9, 175, 237, 255],
-            },
-          },
-        },
-        resolution: 120,
-      },
-      position: {
-        epoch: response.epoch,
-        cartographicDegrees: response.cartographicDegrees,
-      },
-    },
-  ];
-
   return (
     <Viewer full>
-      <CameraFlyTo
-        destination={Cartesian3.fromDegrees(response.position.lon, response.position.lat, 10000000)}
-      />
       <Globe enableLighting />
-      <CzmlDataSource data={czml} />
+      <SateliteOrbital />
     </Viewer>
   );
 };
---
+++ src/components/SateliteOrbital/index.tsx
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import { Cartesian3 } from 'cesium';
+import { CzmlDataSource, CameraFlyTo } from 'resium';
+import { getOrbital, GetOrbitalResponse } from '../../queries/orbitalQuery';
+
+const { useState, useCallback, useEffect } = React;
+
+type OrbitalState = {
+  isLoading: boolean;
+  isError: boolean;
+  response?: GetOrbitalResponse;
+};
+
+const useOrbital = () => {
+  const [state, setState] = useState<OrbitalState>({
+    isLoading: true,
+    isError: false,
+    response: undefined,
+  });
+
+  const fetchData = useCallback(async () => {
+    const res = await getOrbital();
+    if (res.status >= 400) {
+      setState(prevState => {
+        return { ...prevState, isLoading: false, isError: true };
+      });
+    }
+    const data = res.data as GetOrbitalResponse;
+    setState(prevState => {
+      return {
+        ...prevState,
+        isLoading: false,
+        isError: false,
+        response: data,
+      };
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return { ...state };
+};
+
+const SateliteOrbital: React.FC = () => {
+  const { isError, isLoading, response } = useOrbital();
+  if (isLoading) return null;
+  if (isError || !response) return null;
+
+  const czml = [
+    {
+      id: 'document',
+      name: 'CZML',
+      version: '1.0',
+      clock: {
+        currentTime: response.epoch,
+      },
+    },
+    {
+      id: 'Satellite/ISS',
+      name: 'ISS',
+      availability: response.availability,
+      description: '<p>The International Space Station (ISS)</p>',
+      ellipse: {
+        semiMinorAxis: 50000.0,
+        semiMajorAxis: 50000.0,
+        height: response.position.alt,
+        material: {
+          solidColor: {
+            color: {
+              rgba: [9, 175, 237, 255],
+            },
+          },
+        },
+        outline: true, // height must be set for outlines to display
+        outlineColor: {
+          rgba: [255, 255, 255, 255],
+        },
+      },
+      path: {
+        show: [
+          {
+            interval: response.availability,
+            boolean: true,
+          },
+        ],
+        width: 1,
+        material: {
+          solidColor: {
+            color: {
+              rgba: [9, 175, 237, 255],
+            },
+          },
+        },
+        resolution: 120,
+      },
+      position: {
+        epoch: response.epoch,
+        cartographicDegrees: response.cartographicDegrees,
+      },
+    },
+  ];
+
+  return (
+    <>
+      <CameraFlyTo
+        destination={Cartesian3.fromDegrees(response.position.lon, response.position.lat, 10000000)}
+      />
+      <CzmlDataSource data={czml} />
+    </>
+  );
+};
+
+export default SateliteOrbital;
