51d38288727263f569555c1c38146c013cbb0c57
Ushinji <siguma971@gmail.com>
2020-11-01 21:36:15 +0900

    Refactoring get_orbital using Orbital Class.

--- app/views/orbital_view.py
+++ app/views/orbital_view.py
@@ -1,39 +1,28 @@
 import math
 from flask import jsonify
 from datetime import datetime, timedelta
-from pyorbital.orbital import Orbital
 from pyorbital.tlefile import Tle
 from app import application
+from app.models.orbital import Orbital
 
 
 @application.route("/orbitals", methods=["GET"])
 def get_orbital():
     iss_tle = Tle('ISS (ZARYA)', 'app/tle/iss.tle')
-    iss_orbit = Orbital(
-        'ISS (ZARYA)',
-        line1=iss_tle.line1,
-        line2=iss_tle.line2
-    )
+    orbit = Orbital(iss_tle)
     start = datetime.utcnow()
-    cartographic_degrees = []
-    for i in range(100):
-        utc_time = start + timedelta(minutes=i)
-        lon, lat, alt = iss_orbit.get_lonlatalt(utc_time)
-        cartographic_degrees.append(i * 60)
-        cartographic_degrees.append(lon)
-        cartographic_degrees.append(lat)
-        cartographic_degrees.append(alt * 1000)
+    cartographic_degrees = orbit.calculate(start)
 
-    lon, lat, alt = iss_orbit.get_lonlatalt(start)
+    start_position = cartographic_degrees[1:4]
     end = start + timedelta(minutes=100)
 
     return jsonify({
         "epoch": start.strftime("%Y-%m-%dT%H:%M:%SZ"),
         "availability": f'{start.strftime("%Y-%m-%dT%H:%M:%SZ")}/{end.strftime("%Y-%m-%dT%H:%M:%SZ")}',
         "position": {
-            "lon": lon,
-            "lat": lat,
-            "alt": alt * 1000,
+            "lon": start_position[0],
+            "lat": start_position[1],
+            "alt": start_position[2] * 1000,
         },
         "cartographicDegrees": cartographic_degrees,
     }), 200
