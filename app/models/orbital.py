cb40c0a3b5d3ee5a9ead65694a4d55e91227bd03
Ushinji <siguma971@gmail.com>
2020-11-02 23:47:30 +0900

    Fix typo

--- app/models/orbital.py
+++ app/models/orbital.py
@@ -1,10 +1,10 @@
 from datetime import timedelta
-from pyorbital.orbital import PyOrbital
+from pyorbital.orbital import Orbital as PyOrbital
 
 
 class Orbital:
     def __init__(self, tle):
-        self.orbit = Orbital(
+        self.orbit = PyOrbital(
             'ISS (ZARYA)',
             line1=tle.line1,
             line2=tle.line2
@@ -14,7 +14,7 @@ class Orbital:
         cartographic_degrees = []
         for i in range(100):
             utc_time = start + timedelta(minutes=i)
-            lon, lat, alt = iss_orbit.get_lonlatalt(utc_time)
+            lon, lat, alt = self.orbit.get_lonlatalt(utc_time)
             cartographic_degrees.append(i * 60)
             cartographic_degrees.append(lon)
             cartographic_degrees.append(lat)
