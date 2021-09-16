0194a685495e4fd79649857705f3e6615f1cf817
Ushinji <siguma971@gmail.com>
2021-09-16 23:18:59 +0900

    Add CameraFlyTo

--- components/pages/RootPage/index.tsx
+++ components/pages/RootPage/index.tsx
@@ -1,23 +1,31 @@
 import * as React from 'react';
-import { CzmlDataSource, Viewer } from 'resium';
+import { Cartesian3 } from 'cesium';
+import { CzmlDataSource, Viewer, CameraFlyTo } from 'resium';
 import { buildOrbitalCzmlData } from '../../../modules/Cesium';
 import { CartographicDegree } from '../../../modules/Orbit';
 
 type Props = {
   startTime: Date;
   orbital: CartographicDegree[];
-}
+};
 
 const RootPage: React.VFC<Props> = ({ startTime, orbital }) => {
   const czmlData = React.useMemo(() => {
-    return buildOrbitalCzmlData(startTime, orbital)
+    return buildOrbitalCzmlData(startTime, orbital);
   }, []);
 
   return (
     <Viewer full>
+      <CameraFlyTo
+        destination={Cartesian3.fromDegrees(
+          orbital[0].longitude,
+          orbital[0].latitude,
+          20000000
+        )}
+      />
       <CzmlDataSource data={czmlData} />
     </Viewer>
   );
-}
+};
 
 export default RootPage;
