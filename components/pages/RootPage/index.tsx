1f40a3cf3055390962ef310c2da0080c1ae530e4
Ushinji <siguma971@gmail.com>
2021-09-17 21:53:34 +0900

    Add clock animation

--- components/pages/RootPage/index.tsx
+++ components/pages/RootPage/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Cartesian3 } from 'cesium';
-import { CzmlDataSource, Viewer, CameraFlyTo } from 'resium';
+import { CzmlDataSource, Viewer, CameraFlyTo, Clock } from 'resium';
 import { buildOrbitalCzmlData } from 'modules/Cesium';
 import { CartographicDegree } from 'modules/Orbit';
 
@@ -23,6 +23,7 @@ const RootPage: React.VFC<Props> = ({ startTime, orbital }) => {
           20000000
         )}
       />
+      <Clock shouldAnimate />
       <CzmlDataSource data={czmlData} />
     </Viewer>
   );
